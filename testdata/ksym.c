952f3162208724146462601da5144da0d0abbff1
Usama Saqib <usama.saqib@datadoghq.com>
2026-06-29 16:37:00 +0200

    add test bpf programs
    Signed-off-by: Usama Saqib <usama.saqib@datadoghq.com>

--- testdata/ksym.c
+++ testdata/ksym.c
@@ -14,7 +14,7 @@ extern void bpf_init __ksym;
 // Weak ksyms are potentially zero at runtime.
 extern void bpf_trace_run1 __ksym __weak;
 
-__section("socket") int ksym_test() {
+__section("socket") int untyped_ksym_test() {
 	uint32_t i;
 	uint64_t val;
 
@@ -31,8 +31,30 @@ __section("socket") int ksym_test() {
 
 extern void non_existing_symbol __ksym __weak;
 
-__section("socket") int ksym_missing_test() {
+__section("socket") int untyped_ksym_missing_test() {
 	if (&non_existing_symbol == 0) {
+		return 1;
+	}
+	return 0;
+}
+
+extern const int bpf_prog_active __ksym __weak;
+
+__section("socket") int typed_ksym_test() {
+	uint32_t i;
+	uint64_t val;
+
+	i   = 0;
+	val = (uint64_t)&bpf_prog_active;
+	bpf_map_update_elem(&array_map, &i, &val, 0);
+
+	return 0;
+}
+
+extern const struct file_operations__local non_existing_typed_ksym __ksym __weak;
+
+__section("socket") int typed_ksym_missing_test() {
+	if (&non_existing_typed_ksym == 0) {
 		return 1;
 	}
 	return 0;
